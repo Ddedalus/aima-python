5813b6f327141252e716a53205d0ad196a65d9ac
Unknown <rzemieniecki.j@gmail.com>
2018-01-02 17:14:41 +0000

    comments added

--- Our_vacuum.py
+++ Our_vacuum.py
@@ -2,6 +2,5 @@ from agents import *
 
 def OurVacuum():
     Env = TrivialVacuumEnvironment()
-    Env.add_things(TraceAgent(RandomVacuumAgent()))
-    return Env
     Env.add_thing(TraceAgent(RandomVacuumAgent()))
+    return Env #dupa
